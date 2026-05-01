48063e7fe6840c51c87e8839821a183c098ce09e
Pablo Galvan <pablo.galvanf@gmail.com>
2026-04-30 21:34:27 -0500

    fix: resolver warnings de aria-describedby y arreglar definitivamente salto de página al abrir modales

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -40,6 +40,7 @@ const SheetContent = React.forwardRef<
     <SheetOverlay />
     <SheetPrimitive.Content
       ref={ref}
+      aria-describedby={props["aria-describedby"] || undefined}
       className={cn(
         "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 inset-y-0 h-full w-3/4 sm:max-w-sm",
         side === "left"
