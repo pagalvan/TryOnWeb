756a434c97debc1669c1f2dd4c74e76ac606206e
hx4m099 <hricardolopez@unicesar.edu.co>
2026-05-19 18:48:49 -0500

    fix CSP to allow blob scripts for Camera Kit

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data: https:; connect-src 'self' https: wss:; media-src 'self' data: blob: https:; frame-src 'self' https:;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' blob:; worker-src 'self' blob:; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data: https:; connect-src 'self' https: wss:; media-src 'self' data: blob: https:; frame-src 'self' https:;"
           },
           {
             key: 'X-Content-Type-Options',
